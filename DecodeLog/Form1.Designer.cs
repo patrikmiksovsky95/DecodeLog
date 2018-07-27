babb3f427ecc8f17b5dcb2717f0fc14b2c73e592
Patrik Mikšovský <patrik@mewssystems.com>
2018-07-27 09:54:34 +0200

    init

---
+++ DecodeLog/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace DecodeLog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnDecodeLog = new System.Windows.Forms.Button();
+            this.btnFormatXML = new System.Windows.Forms.Button();
+            this.btnFormatJson = new System.Windows.Forms.Button();
+            this.message = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(1032, 483);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnDecodeLog
+            // 
+            this.btnDecodeLog.Location = new System.Drawing.Point(711, 501);
+            this.btnDecodeLog.Name = "btnDecodeLog";
+            this.btnDecodeLog.Size = new System.Drawing.Size(93, 23);
+            this.btnDecodeLog.TabIndex = 1;
+            this.btnDecodeLog.Text = "Decode log";
+            this.btnDecodeLog.UseVisualStyleBackColor = true;
+            this.btnDecodeLog.Click += new System.EventHandler(this.decodeLog_Click);
+            // 
+            // btnFormatXML
+            // 
+            this.btnFormatXML.Location = new System.Drawing.Point(831, 501);
+            this.btnFormatXML.Name = "btnFormatXML";
+            this.btnFormatXML.Size = new System.Drawing.Size(87, 23);
+            this.btnFormatXML.TabIndex = 2;
+            this.btnFormatXML.Text = "Format XML";
+            this.btnFormatXML.UseVisualStyleBackColor = true;
+            this.btnFormatXML.Click += new System.EventHandler(this.btnFormatXML_Click);
+            // 
+            // btnFormatJson
+            // 
+            this.btnFormatJson.Location = new System.Drawing.Point(936, 501);
+            this.btnFormatJson.Name = "btnFormatJson";
+            this.btnFormatJson.Size = new System.Drawing.Size(108, 23);
+            this.btnFormatJson.TabIndex = 3;
+            this.btnFormatJson.Text = "Format JSON";
+            this.btnFormatJson.UseVisualStyleBackColor = true;
+            this.btnFormatJson.Click += new System.EventHandler(this.btnFormatJson_Click);
+            // 
+            // message
+            // 
+            this.message.AutoSize = true;
+            this.message.Location = new System.Drawing.Point(12, 544);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(0, 13);
+            this.message.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1075, 595);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.btnFormatJson);
+            this.Controls.Add(this.btnFormatXML);
+            this.Controls.Add(this.btnDecodeLog);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Best tool for xml json ever!!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnDecodeLog;
+        private System.Windows.Forms.Button btnFormatXML;
+        private System.Windows.Forms.Button btnFormatJson;
+        private System.Windows.Forms.Label message;
+    }
+}
+
---
+++ DecodeLog/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Soap;
+using Newtonsoft.Json;
+
+namespace DecodeLog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ResetMessage()
+        {
+            message.Text = "";
+        }
+
+        private void decodeLog_Click(object sender, EventArgs e)
+        {
+            ResetMessage();
+
+            var log = textBox1.Text;
+            textBox1.Text = log.DecodeXmlLog().DecodeJsonLog().NormalizeLog();
+        }
+
+
+        private void btnFormatXML_Click(object sender, EventArgs e)
+        {
+            ResetMessage();
+
+            string result = "";
+
+            var mStream = new MemoryStream();
+            var writer = new XmlTextWriter(mStream, Encoding.Unicode);
+            var document = new XmlDocument();
+
+            try
+            {
+                document.LoadXml(textBox1.Text);
+
+                writer.Formatting = System.Xml.Formatting.Indented;
+
+                document.WriteContentTo(writer);
+                writer.Flush();
+                mStream.Flush();
+
+                mStream.Position = 0;
+
+                StreamReader sReader = new StreamReader(mStream);
+
+                string formattedXml = sReader.ReadToEnd();
+
+                result = formattedXml;
+            }
+            catch (XmlException ex)
+            {
+                textBox1.Text = $"Invalid XML object. {ex}";
+            }
+
+            mStream.Close();
+            writer.Close();
+
+            textBox1.Text = result;
+        }
+
+
+        private void btnFormatJson_Click(object sender, EventArgs e)
+        {
+            ResetMessage();
+
+            try
+            {
+                textBox1.Text = FormatJson(textBox1.Text);
+            }
+            catch (Exception ex)
+            {
+                message.Text = $"Invalid JSON object. {ex}";
+            }
+        }
+
+        private const string INDENT_STRING = "    ";
+        public string FormatJson(string str)
+        {
+            var indent = 0;
+            var quoted = false;
+            var sb = new StringBuilder();
+            for (var i = 0; i < str.Length; i++)
+            {
+                var ch = str[i];
+                switch (ch)
+                {
+                    case '{':
+                    case '[':
+                        sb.Append(ch);
+                        if (!quoted)
+                        {
+                            sb.AppendLine();
+                            Enumerable.Range(0, ++indent).ForEach(item => sb.Append(INDENT_STRING));
+                        }
+                        break;
+                    case '}':
+                    case ']':
+                        if (!quoted)
+                        {
+                            sb.AppendLine();
+                            Enumerable.Range(0, --indent).ForEach(item => sb.Append(INDENT_STRING));
+                        }
+                        sb.Append(ch);
+                        break;
+                    case '"':
+                        sb.Append(ch);
+                        bool escaped = false;
+                        var index = i;
+                        while (index > 0 && str[--index] == '\\')
+                            escaped = !escaped;
+                        if (!escaped)
+                            quoted = !quoted;
+                        break;
+                    case ',':
+                        sb.Append(ch);
+                        if (!quoted)
+                        {
+                            sb.AppendLine();
+                            Enumerable.Range(0, indent).ForEach(item => sb.Append(INDENT_STRING));
+                        }
+                        break;
+                    case ':':
+                        sb.Append(ch);
+                        if (!quoted)
+                            sb.Append(" ");
+                        break;
+                    default:
+                        sb.Append(ch);
+                        break;
+                }
+            }
+            return sb.ToString();
+        }
+    }
+
+    public static class logExtensions
+    {
+        public static string DecodeXmlLog(this string log)
+        {
+            return new Regex("\\[\\d\\d.*\\\"message\\\":\"").Replace(log, "");
+
+        }
+        public static string DecodeJsonLog(this string log)
+        {
+            log = new Regex("\\[\\d\\d.*\\\"Parameters\\\":").Replace(log, "");
+            return new Regex(",\\\"Exception.*}").Replace(log, "");
+        }
+        public static string NormalizeLog(this string log)
+        {
+            return log.Replace("\\r\\n", "").Replace("\\n", "").Replace("\\\"", "\"").Replace("\"}", "").Trim().Trim('"');
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> ie, Action<T> action)
+        {
+            foreach (var i in ie)
+            {
+                action(i);
+            }
+        }
+    }
+}   
